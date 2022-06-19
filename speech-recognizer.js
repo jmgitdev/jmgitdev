8f4de530e7a45030f21c20b79ae5b4f45d3ba4a3
jmgitdev <107635054+jmgitdev@users.noreply.github.com>
2022-06-19 16:16:26 +0200

    Update speech-recognizer.js

--- speech-recognizer.js
+++ speech-recognizer.js
@@ -1,6 +1,43 @@
 if ("webkitSpeechRecognition" in window) {
 
   console.log("Speech Recognition available")
+  let speech_recognizer = new webkitSpeechRecognition();
+  
+  speech_recognizer.continuous = false;
+  
+  speech_recognizer.interimResults = true;
+  
+  speech_recognizer.lang = "es-ES"
+  
+  speech_recognizer.onstart = () => 
+  {
+    console.log("Escuchando...");
+  };
+  
+  speech_recognizer.onend = () => 
+  {
+    console.log("Finalizado.");
+  };
+  
+  
+  speech_recognizer.onresult = (event) => 
+  {
+    // Create the interim transcript string locally because we don't want it to persist like final transcript
+    let interim_transcript = "";
+
+    // Loop through the results from the speech recognition object.
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    
+    console.log(final_transcript);
+  };
+  
 
 } else {
   console.log("Speech Recognition Not Available")
